aca90b3e8df4ee34724578bfc7c310a4ae14650b
Mohamed Wael <hfhybujj@gmail.com>
2024-02-01 00:54:58 +0200

    Update Index.cshtml and AssemblyInfo.cs

--- FirstWebApp/wwwroot/js/site.js
+++ FirstWebApp/wwwroot/js/site.js
@@ -3,4 +3,39 @@
 
 // Write your JavaScript code.
 
+// make the navbar transparent when it is on the top
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    if (window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar--dark');
+    } else {
+        navbar.classList.remove('navbar--dark');
+    }
+});
+
+// handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    navbarMenu.classList.remove('open');
+    scrollIntoView(link);
+});
+
+// when I hover on the navbar menu, the menu should be open
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+});
+
+// handle click on "contact me" button on home
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+    scrollIntoView('#contact');
+});
+
 
