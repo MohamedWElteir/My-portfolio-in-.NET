04565e71b987b66ad9889e2644dd687b5acb6842
Mohamed Wael <hfhybujj@gmail.com>
2024-02-01 00:12:53 +0200

    commit

--- FirstWebApp/obj/Debug/net8.0/FirstWebApp.AssemblyInfo.cs
+++ FirstWebApp/obj/Debug/net8.0/FirstWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6fbc79a7818ff71a198a404debd09a6a63785ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a8b5e126e18841827c7b5f5d7835f57f442a3df")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
